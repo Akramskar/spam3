959f6b72767976c84a949b05c8764b2c3f75bde5
Akramskar <47114339+Akramskar@users.noreply.github.com>
2019-01-28 17:30:05 +0100

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("539481183579406337")
+setInterval(function() {
+channel.send(`spin around brother`);
+}, 30)
+})
+
+client.login(process.env.BOT_TOKEN);
